196265d71d3992e4ce317804ec3051bc5258aaff
Sourav Kundu <skundu.dev@gmail.com>
2024-07-08 12:33:43 -0500

    tightened up sg #6

--- security_group.tf
+++ security_group.tf
@@ -28,6 +28,6 @@ resource "aws_security_group_rule" "egress_custom_sg" {
   from_port         = 0
   to_port           = 0
   protocol          = "-1"
-  cidr_blocks       = ["0.0.0.0/0"]
+  cidr_blocks       = [var.vpc_cidr]
   security_group_id = aws_security_group.custom_sg.id
 }
