02fd75138957afc0258e611bef6c246f7027af63
Sourav Kundu <skundu.dev@gmail.com>
2025-03-03 12:05:55 -0600

    refering vpc module

--- network.tf
+++ network.tf
@@ -1,50 +1,15 @@
-
-resource "aws_vpc" "this" {
-  #checkov:skip=CKV2_AWS_11: This is non prod and hence disabled.
-  cidr_block           = var.vpc_cidr
-  enable_dns_hostnames = true
-  enable_dns_support   = true
-  tags = {
-    "Name" = "${var.name}"
-  }
-}
-resource "aws_subnet" "public" {
-  count             = length(var.subnet_cidr_public)
-  vpc_id            = aws_vpc.this.id
-  cidr_block        = var.subnet_cidr_public[count.index]
-  availability_zone = var.availability_zone[count.index]
-  tags = {
-    "Name" = "${var.name}-public-${count.index + 1}"
-  }
-}
-resource "aws_route_table" "this_rt" {
-  vpc_id = aws_vpc.this.id
-  tags = {
-    "Name" = "${var.name}-route-table"
-  }
-}
-resource "aws_route_table_association" "public" {
-  count          = length(var.subnet_cidr_public)
-  subnet_id      = element(aws_subnet.public.*.id, count.index)
-  route_table_id = aws_route_table.this_rt.id
-}
-resource "aws_subnet" "private" {
-  count             = length(var.subnet_cidr_public)
-  vpc_id            = aws_vpc.this.id
-  cidr_block        = var.subnet_cidr_private[count.index]
-  availability_zone = var.availability_zone[count.index]
-  tags = {
-    "Name" = "${var.name}-private-${count.index + 1}"
-  }
-}
-resource "aws_route_table" "this_rt_private" {
-  vpc_id = aws_vpc.this.id
-  tags = {
-    "Name" = "${var.name}-route-table-private"
-  }
-}
-resource "aws_route_table_association" "private" {
-  count          = length(var.subnet_cidr_private)
-  subnet_id      = element(aws_subnet.private.*.id, count.index)
-  route_table_id = aws_route_table.this_rt_private.id
+#https://registry.terraform.io/modules/kunduso/vpc/aws/1.0.3
+module "vpc" {
+  source                  = "kunduso/vpc/aws"
+  version                 = "1.0.3"
+  region                  = var.region
+  enable_dns_hostnames    = true
+  enable_dns_support      = true
+  enable_flow_log         = true
+  enable_internet_gateway = false
+  enable_nat_gateway      = false
+  vpc_name                = var.name
+  vpc_cidr                = var.vpc_cidr
+  subnet_cidr_public      = var.subnet_cidr_public
+  subnet_cidr_private     = var.subnet_cidr_private
 }
