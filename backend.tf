aa885d28b602a5bc5514dbb7315fe765e6f13327
Sourav Kundu <skundu.dev@gmail.com>
2025-03-03 16:57:55 -0600

    fix format

--- backend.tf
+++ backend.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "s3" {
-    bucket  = "kunduso-terraform-remote-bucket"
-    encrypt = true
-    key     = "tf/amazon-elasticache-serverless/terraform.tfstate"
-    region  = "us-east-2"
+    bucket       = "kunduso-terraform-remote-bucket"
+    encrypt      = true
+    key          = "tf/amazon-elasticache-serverless/terraform.tfstate"
+    region       = "us-east-2"
     use_lockfile = true
   }
 }
